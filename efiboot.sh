68d383f8623fd9ec15d61e8588e01d0a771011af
Kirill Pshenichnyi <pshcyrill@mail.ru>
2025-05-26 12:15:02 +0300

    efiboot.sh: add --exec option and string style edit

--- efiboot.sh
+++ efiboot.sh
@@ -2,17 +2,19 @@
 
 # Простой скрипт формирующий стркочку для efibootmgr, чтобы загружать ядро
 # в режиме EFI Stub без использования загрузчика
+# Если передан параметр --exec то строчка выполнится
 
-SUFFIX='-workpc'
-LABEL='"ArchLinux'${SUFFIX}' EFI stub"'
+if [[ -z $SUFFIX ]]; then SUFFIX='-workpc'; fi
+if [[ -z $LINUX_NAME ]]; then LINUX_NAME="ArchLinux"; fi
+LABEL='"'${LINUX_NAME}${SUFFIX}' EFI stub"'
 
 KERNEL_PATH='\\arch\\vmlinuz-linux'${SUFFIX}
 INITRDS='\arch\intel-ucode.img \arch\initramfs-linux'${SUFFIX}'.img'
 
-ESP_DISK='/dev/sda'
-ESP_PART='1'
-ROOT_UUID='f4e983c2-595f-400e-9aee-56981fdc565e'
-SWAP_UUID='5beb6a95-a533-4234-ad57-2e80e4020820'
+if [[ -z ${ESP_DISK} ]]; then ESP_DISK='/dev/sda'; fi
+if [[ -z ${ESP_PART} ]]; then ESP_PART='1'; fi
+if [[ -z ${ROOT_UUID} ]]; then ROOT_UUID='f4e983c2-595f-400e-9aee-56981fdc565e'; fi
+if [[ -z ${SWAP_UUID} ]]; then SWAP_UUID='5beb6a95-a533-4234-ad57-2e80e4020820'; fi
 
 for initrd in ${INITRDS}
 do
@@ -24,4 +26,13 @@ KERN_CMDLINE=${INITRD}' root=UUID='${ROOT_UUID}' rw resume=UUID='${SWAP_UUID}' c
 echo "label: "${LABEL}
 echo "loader: "${KERNEL_PATH}
 echo "cmdline: "${KERN_CMDLINE}
-echo efibootmgr -c -L ${LABEL} -l ${KERNEL_PATH} -d ${ESP_DISK} -p ${ESP_PART} -u \'${KERN_CMDLINE}\'
+FULL_COMMAND=efibootmgr\ -c\ -L\ ${LABEL}\ -l\ ${KERNEL_PATH}\ -d\ ${ESP_DISK}\ -p\ ${ESP_PART}\ -u\ \'${KERN_CMDLINE}\'
+
+echo -e "Full command: \033[1;96m"  # Cyan color
+echo ${FULL_COMMAND}
+echo -e "\033[0m"
+
+if [[ "$1" == "--exec" ]];
+then
+	${FULL_COMMAND}
+fi
